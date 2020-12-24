b1566e95f47737d20b8c09f35bb39f131df97d18
Matth-Ben <matthias.benoit@outlook.fr>
2020-12-24 11:28:23 +0100

    Modification footer

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="contact">
     <h2 class="contact--title">Contact</h2>
+    <hr class="separator grey mt-0">
     <div class="row">
       <div class="col4">
-        <div class="contact--rs">
-          <div class="grid grid2">
-            <div class="grid-item">
-              <div class="svg--insta">
-                <a href=""><i class="fab fa-instagram-square"></i></a>
-              </div>
-            </div>
-            <div class="grid-item">
-              <div class="svg--linkedin">
-                <a href=""><i class="fab fa-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
+        <div class="contact--info">
+          <h3 class="contact--info--item">
+            <a class="contact--info--item--link" href="https://goo.gl/maps/kiEZiQ9E9gjGEhDY6" target="_blank">
+              4 Rue Louis Braille, 69100 Villeurbanne
+            </a>
+          </h3>
+          <h3 class="contact--info--item">
+            <a class="contact--info--item--link" href="tel:0619083980">
+              06 19 08 39 80
+            </a>
+          </h3>
+          <h3 class="contact--info--item">
+            <a class="contact--info--item--link" href="mailto:victorravinet@gmail.com">
+            victorravinet@gmail.com
+            </a>
+          </h3>
         </div>
       </div>
-      <div class="col8">
-        <div class="contact--email">
-          <h3 class="contact--email--item"><a class="contact--email--item--link" href="mailto:victorravinet@gmail.com">victorravinet@gmail.com</a></h3>
+      <div class="col4">
+        <div class="contact--rs">
+          <a href="https://www.instagram.com/victorrvnt/" target="_blank"><i class="fab fa-instagram-square"></i></a>
+          <a href="https://www.linkedin.com/in/victor-ravinet-59aba3172/" target="_blank"><i class="fab fa-linkedin"></i></a>
         </div>
       </div>
     </div>
+    <hr class="separator grey">
   </div>
 </template>
 
